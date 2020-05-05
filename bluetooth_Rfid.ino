c87bad318ff0e6668cf81a26899eef34f99bac3b
Samuel Eljoy <54344839+crimson-eclipse@users.noreply.github.com>
2020-05-05 19:56:38 +0700

    Add files via upload

---
+++ bluetooth_Rfid.ino
@@ -0,0 +1,79 @@
+//RFID
+#include <SPI.h>
+#include <MFRC522.h>
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+ 
+#define SS_PIN 5
+#define RST_PIN 13
+#define LED_G 27 //lampu biru
+#define LED_R 14 //lampu merah
+
+BluetoothSerial SerialBT;
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(115200);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "80 B3 8C BB") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(500);
+    digitalWrite(LED_G, HIGH);
+    SerialBT.println("AKSES DITERIMA");
+    
+    SerialBT.println("ID KARTU ANDA : " + content.substring(1));
+    delay(1000);
+    digitalWrite(LED_G, LOW);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    digitalWrite(LED_R, HIGH);
+    SerialBT.println("AKSES DITOLAK");
+    SerialBT.println("ID KARTU ANDA : " + content.substring(1));
+    delay(1000);
+    digitalWrite(LED_R, LOW);
+    
+  }
+}
